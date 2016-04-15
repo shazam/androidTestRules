b84d00017d22da81fbf3d8533f2a4037596fdf75
Iordanis Giannakakis <iordanis.giannakakis@shazam.com>
2016-04-15 14:54:27 +0100

    Adding model rule.

---
+++ src/main/java/com/shazam/android/test/rules/Model.java
@@ -0,0 +1,24 @@
+package com.shazam.android.test.rules;
+
+import android.os.Build;
+
+public class Model {
+
+    public static class Nexus10 implements Condition {
+        @Override
+        public boolean isSatisfied() {
+            return modelEquals("Nexus 10");
+        }
+    }
+
+    public static class Nexus4 implements Condition {
+        @Override
+        public boolean isSatisfied() {
+            return modelEquals("Nexus 4");
+        }
+    }
+
+    private static boolean modelEquals(String other) {
+        return Build.MODEL.equals(other);
+    }
+}

6c0f795221725f61a0c4e69e126197eaa8f3d364
Iordanis Giannakakis <iordanis.giannakakis@shazam.com>
2016-04-15 14:56:13 +0100

    Adding device rule.

---
+++ src/main/java/com/shazam/android/test/rules/Device.java
@@ -0,0 +1,17 @@
+package com.shazam.android.test.rules;
+
+import android.os.Build;
+
+public class Device {
+    public static class Genymotion implements Condition {
+
+        @Override
+        public boolean isSatisfied() {
+            return deviceEquals("vbox86p");
+        }
+    }
+
+    private static boolean deviceEquals(String device) {
+        return Build.DEVICE.equals(device);
+    }
+}
